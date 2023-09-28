47b750d77ec08d24897ebd8adac75e6e8b796603
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2023-09-28 14:53:24 -0300

    feat: add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,87 @@
 import { IntrospectAndCompose } from '@apollo/gateway';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { RemoteGraphQLDataSource } from '@apollo/gateway';
+import {
+  Module,
+  BadRequestException,
+  HttpStatus,
+  HttpException,
+  UnauthorizedException,
+  MiddlewareConsumer,
+} from '@nestjs/common';
+import { INVALID_AUTH_TOKEN, INVALID_BEARER_TOKEN } from './app.constants';
 import { GraphQLModule } from '@nestjs/graphql';
+import { verify, decode } from 'jsonwebtoken';
+import { graphqlUploadExpress } from 'graphql-upload';
 
+const getToken = (authToken: string): string => {
+  const match = authToken.match(/^Bearer (.*)$/);
+  if (!match || match.length < 2) {
+    throw new HttpException(
+      { message: INVALID_BEARER_TOKEN },
+      HttpStatus.UNAUTHORIZED,
+    );
+  }
+  return match[1];
+};
 
+const decodeToken = (tokenString: string) => {
+  const decoded = verify(tokenString, 'secret');
+  if (!decoded) {
+    throw new HttpException(
+      { message: INVALID_AUTH_TOKEN },
+      HttpStatus.UNAUTHORIZED,
+    );
+  }
+  return decoded;
+};
+const handleAuth = ({ req }) => {
+  try {
+    if (req.headers.authorization) {
+      const token = getToken(req.headers.authorization);
+      const decoded: any = decodeToken(token);
+      return {
+        userId: decoded.userId,
+        permissions: decoded.permissions,
+        authorization: `${req.headers.authorization}`,
+      };
+    }
+  } catch (err) {
+    throw new UnauthorizedException(
+      'User unauthorized with invalid authorization Headers',
+    );
+  }
+};
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      server: {
+        context: handleAuth,
+      },
       driver: ApolloGatewayDriver,
-      gateway:{
-        supergraphSdl: new IntrospectAndCompose ({
-          subgraphs:[
-            {name: 'users', url: 'http://localhost:4001/graphql'},
-          ]
-        })
-      }
-    })
+      gateway: {
+        buildService: ({ name, url }) => {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }: any) {
+              request.http.headers.set('userId', context.userId);
+              // for now pass authorization also
+              request.http.headers.set('authorization', context.authorization);
+              request.http.headers.set('permissions', context.permissions);
+            },
+          });
+        },
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'user', url: 'http://localhost:4001/graphql' },
+          ],
+        }),
+      },
+    }),
   ],
-  controllers: [],
-  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(graphqlUploadExpress()).forRoutes('graphql');
+  }
+}
